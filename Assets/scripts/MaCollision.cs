097dde569b6790ab41477abec41979199c1efda8
Paulrbo <paul.herbeau01@gmail.com>
2022-10-03 23:19:22 +0200

    collision

--- Assets/scripts/MaCollision.cs
+++ Assets/scripts/MaCollision.cs
@@ -1,20 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mail;
 using UnityEngine;
 
 public class MaCollision : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Mur"))
+    
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Cible"))
         {
-            Destroy(collision.gameObject);
-            Debug.Log("Collision");
-        }
-        else if (collision.gameObject.layer == LayerMask.NameToLayer("Cible"))
-        {
-            Destroy(collision.gameObject);
-            Debug.Log("BOOM");
+            //Destroy(gameObject);
+            Destroy(collision.gameObject,2f);
         }
     }
 }
